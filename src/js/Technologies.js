3210be98cede393fbcb370953aa30a117e0e85ec
VianaMateus <mateus.maschietto@sga.pucminas.br>
2024-05-27 12:12:19 -0300

    feature: add tecnologies section

--- src/js/Technologies.js
+++ src/js/Technologies.js
@@ -1,3 +1,50 @@
+const techList = {
+    'FrontEnd': [
+        {
+            tech: 'React',
+            imgSrc: ''
+        },
+        {
+            tech: 'Django',
+            imgSrc: ''
+        },
+        {
+            tech: 'JavaScript',
+            imgSrc: ''
+        },
+        {
+            tech: 'TypeScript',
+            imgSrc: ''
+        },
+        {
+            tech: 'HTML',
+            imgSrc: ''
+        },
+        {
+            tech: 'CSS',
+            imgSrc: ''
+        }
+    ],
+    BackEnd: [
+        {
+            tech: 'Django',
+            imgSrc: ''
+        },
+        {
+            tech: 'Node.js',
+            imgSrc: ''
+        },
+        {
+            tech: 'C',
+            imgSrc: ''
+        },
+        {
+            tech: 'C++',
+            imgSrc: ''
+        }
+    ]
+}
+
 class Technologies extends HTMLElement {
     constructor() {
         super();
@@ -7,6 +54,15 @@ class Technologies extends HTMLElement {
     build() {
         const shadow = this.attachShadow({mode: 'open'});
         shadow.appendChild(this.styles());
+
+        const technologies = this.createTechnologies();
+
+        for(let key in techList) {
+            const section = this.createSection(key);
+            technologies.appendChild(section);
+        }
+
+        shadow.appendChild(technologies);
     }
 
     styles() {
@@ -19,6 +75,24 @@ class Technologies extends HTMLElement {
         `
 
         return style;
+    }
+
+    createTechnologies() {
+        const tech = document.createElement('div');
+        tech.classList.add('technologies');
+        return tech;
+    }
+
+    createSection(_sectionName) {
+        const section = document.createElement('div');
+        section.classList.add(_sectionName);
+
+        const title = document.createElement('p');
+        title.classList.add('section-title');
+        title.textContent = _sectionName;
+        section.appendChild(title);
+
+        return section;
     }
 }
 
--- src/js/script.js
+++ src/js/script.js
@@ -2,17 +2,17 @@ const navBarContainers = {
     'About Me': 'about-me-container',
     'Projects': 'projects-container',
     'Technologies': 'technologies-container'
-}
+};
 
 document.getElementById('navbar').addEventListener('click', (event) => {
-    console.log('entrou')
     if(event.target.tagName === 'LI' || event.target.closest('li')) {
         const listItem = event.target.closest('li');
-        // Retrieve the text value of the clicked item
         const textValue = listItem.querySelector('p').textContent;
-        
 
         const item = document.createElement(navBarContainers[textValue]);
-        document.getElementById('section-container').appendChild(item);
+
+        const section = document.getElementById('section-container');
+        section.innerHTML = '';
+        section.appendChild(item);
     }
 });
